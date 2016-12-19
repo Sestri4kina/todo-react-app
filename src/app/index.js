fada2e5466a6d96c9b4b7f254af6be7303d4643c
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-19 22:57:24 +0200

    Add some props

--- src/app/index.js
+++ src/app/index.js
@@ -3,9 +3,17 @@ var ReactDOM = require('react-dom');
 
 var TodoComponent = React.createClass({
     render: function(){
-        return <h1>Hello there!</h1>;
+        return(
+            <div>
+                <h1>Want to make a todo-list?</h1>
+                <p>{this.props.mssg}</p>
+                <p>Title: {this.props.test.title}</p>
+                <p>Text: {this.props.test.text}</p>
+            </div>
+        );
     }
 });
 
-ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
+var testMessage = {title: "Message", text: "This props works just fine."};
+ReactDOM.render(<TodoComponent mssg="Testing props so far." test={testMessage}/>, document.getElementById('todo-wrapper'));
 
