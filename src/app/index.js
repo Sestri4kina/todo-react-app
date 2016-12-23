f7009c56e55439c1231fd5d76ba390fa6b1c9070
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-23 15:02:41 +0200

    Add changes to the state

--- src/app/index.js
+++ src/app/index.js
@@ -4,13 +4,20 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
     getInitialState: function(){
         return {
-            todos: ['solve a task', 'go for a run', 'write some more code']
+            todos: ['solve a task', 'go for a run', 'write some more code'],
+            age: 29
         }
     },
     render: function(){
+        var ager = setTimeout(function(){
+            this.setState({
+                age: 25
+            });
+        }.bind(this), 5000);
         return(
             <div id="todo-list">
                 <p>My plans for a day</p>
+                <p>Ok, let's see how {this.state.age} years person spend a day </p>
                 <ul>
                     <li>{this.state.todos[0]}</li>
                     <li>{this.state.todos[1]}</li>
