bfba8edc549e2cd958bded4ad237e8d721fe0077
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-23 14:48:18 +0200

    Add initial states

--- src/app/index.js
+++ src/app/index.js
@@ -2,18 +2,25 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: ['solve a task', 'go for a run', 'write some more code']
+        }
+    },
     render: function(){
         return(
-            <div>
-                <h1>Want to make a todo-list?</h1>
-                <p>{this.props.mssg}</p>
-                <p>Title: {this.props.test.title}</p>
-                <p>Text: {this.props.test.text}</p>
+            <div id="todo-list">
+                <p>My plans for a day</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
             </div>
         );
-    }
+    }//render
 });
 
-var testMessage = {title: "Message", text: "This props works just fine."};
-ReactDOM.render(<TodoComponent mssg="Testing props so far." test={testMessage}/>, document.getElementById('todo-wrapper'));
+
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
 
