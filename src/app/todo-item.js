f4c53946087ab0a45bc90a2cc5dbecafa1f572cd
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-05-12 21:12:49 +0300

    D imported  'remove' icon

--- src/app/todo-item.js
+++ src/app/todo-item.js
@@ -3,8 +3,8 @@
  */
 import React from 'react';
 require('./css/todo-item.css');
-import Remove from 'react-icons/lib/fa/minus';
 import Check from 'react-icons/lib/fa/check';
+
 //create TodoItem
 export default class TodoItem extends React.Component {
   constructor(props) {
@@ -18,7 +18,7 @@ export default class TodoItem extends React.Component {
                 <div className="todo-item">
                     <span className={this.props.item.completed ? "item-name item-completed" : "item-name"}>{this.props.item.value}</span>
                     <span className="item-check" onClick={this.handleCheck}> <Check /> </span>
-                    <span className="item-remove" onClick={this.handleDelete}> <Remove /> </span>
+                    <span className="item-remove" onClick={this.handleDelete}> &times;  </span>
                 </div>
             </li>
         );
